b99232792df76de55c9f24e9d667ea14c04d38dd
EmilyErickson <emilyerickson2002@gmail.com>
2024-04-10 17:14:20 -0500

    Create coordinate calculation function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,52 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ImageDisplay from './components/ImageDisplay'
+import CharacterList from './components/CharacterList'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
+
+  const calculateScaledCoordinates = ({ x, y }) => {
+    const refScreenWidth = 1280;
+    const refScreenHeight = 800;
+
+
+    const currentScreenWidth = window.screen.availWidth;
+    const currentScreenHeight = window.screen.availHeight;
+
+    console.log(window.screen)
+
+    const widthScale = currentScreenWidth / refScreenWidth;
+    const heightScale = currentScreenHeight / refScreenHeight;
+
+    const scaledX = Math.round(x * widthScale);
+    const scaledY = Math.round(y * heightScale);
+
+    return { x: scaledX, y: scaledY };
+  }
+
+  const checkCharacterLocation = (character, position) => {
+    let x = position.x
+    let y = position.y
+    const scaledPosition = calculateScaledCoordinates({ x, y });
+    console.log(x, y)
+    //Send to backend
+    console.log(character, "scaled", scaledPosition)
+
+    setSelectedCharacter(null);
+  };
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Where&apos;s Waldo</h1>
+      <CharacterList
+        characters={['Waldo', 'The Wizard', 'Wilma', "Odlaw"]}
+      />
+      <ImageDisplay selectedCharacter={selectedCharacter} checkCharacterLocation={checkCharacterLocation}/>
+    </div>
+  );
+
 }
 
 export default App
