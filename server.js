21647aa7ce603f2ed7df44932336deee6924d4d2
EmilyErickson <emilyerickson2002@gmail.com>
2024-05-06 16:56:37 -0500

    Update checkCharacterLocation to work for all characters

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require("express");
 const cors = require("cors");
 
@@ -12,9 +11,6 @@ app.use(
   })
 );
 
-// const express = require("express");
-
-// const app = express();
 const port = 3001; // Choose a port for your server
 
 app.use(bodyParser.json());
@@ -25,17 +21,18 @@ app.post("/checkCharacterLocation", (req, res) => {
   const x = position.x;
   const y = position.y;
 
-  const characterX = 80;
-  const characterY = 739;
+  const characterX = character.xCoordinate;
+  const characterY = character.yCoordinate;
   let characterFound = null;
   // Your logic to check if the character is within the target box
   if (
-    characterX <= x + 50 &&
-    characterX >= x - 50 &&
-    characterY <= y + 50 &&
-    characterY >= y - 50
+    characterX <= x + 25 &&
+    characterX >= x - 25 &&
+    characterY <= y + 25 &&
+    characterY >= y - 25
   ) {
     console.log("found");
+    character.isFound = true;
     characterFound = character;
   } else {
     console.log("xy", x, y);
